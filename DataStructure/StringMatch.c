30351a4d2fa5d97d1486e3a9edf4778c0401ba1c
endinferno <a9214863@sina.com>
2018-04-14 14:04:30 +0800

    Update StringMatch.c

--- DataStructure/StringMatch.c
+++ DataStructure/StringMatch.c
@@ -3,7 +3,7 @@
  * @Description: String Matching
  * @Date: 2018-04-14 11:13:33 
  * @Last Modified by: endinferno.DataStructure
- * @Last Modified time: 2018-04-14 12:14:32
+ * @Last Modified time: 2018-04-14 14:04:15
  */
 
 #include <stdio.h>
@@ -11,6 +11,7 @@
 
 int IndexSimple(char *S, char *T, int pos);
 void GetNext(char *str, int *next);
+int IndexKMP(char *S, char *T);
 
 int IndexSimple(char *S, char *T, int pos)
 {
@@ -35,23 +36,43 @@ int IndexSimple(char *S, char *T, int pos)
         return -1;
 }
 
-void GetNext(char *str, int *next)
+void GetNext(char *T, int *next)
 {
-    int i = 1, j = 0;
+    int i = 1;
     next[1] = 0;
-    while (i < strlen(str))
+    int j = 0;
+    while (i < strlen(T))
     {
-        if (j == 0 || str[i] == str[j])
+        if (j == 0 || T[i - 1] == T[j - 1])
         {
             i++;
             j++;
             next[i] = j;
         }
         else
+        {
             j = next[j];
+        }
     }
 }
 
-int IndexKMP(char *S, char *T, int pos)
+int IndexKMP(char *S, char *T)
 {
+    int next[10];
+    GetNext(T, next);
+    int i = 1;
+    int j = 1;
+    while (i <= strlen(S) && j <= strlen(T))
+    {
+        if (j == 0 || S[i - 1] == T[j - 1])
+        {
+            i++;
+            j++;
+        }
+        else
+            j = next[j];
+    }
+    if (j > strlen(T))
+        return i - (int)strlen(T) - 1;
+    return -1;
 }
